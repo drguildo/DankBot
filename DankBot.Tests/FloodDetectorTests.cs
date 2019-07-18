986ad13ed0978b3a9ffedc52bca1e2af496e6388
Simon Morgan <sjm@sjm.io>
2019-07-18 19:22:24 +0100

    Add a project for tests

---
+++ DankBot.Tests/FloodDetectorTests.cs
@@ -0,0 +1,52 @@
+namespace DankBot.Tests
+{
+    using System;
+    using System.Linq;
+
+    using Xunit;
+
+    public class FloodDetectorTests
+    {
+        private const int _threshold = 10;
+        private const int _id = 12345;
+
+        [Fact]
+        public void FloodDetector_AddLessThanThreshold_ShouldNotBeFlagged()
+        {
+            var floodDetector = new FloodDetector(60, _threshold);
+
+            foreach (var i in Enumerable.Range(1, _threshold - 1))
+            {
+                floodDetector.Add(_id, DateTime.Now);
+            }
+
+            Assert.False(floodDetector.IsFlooding(_id));
+        }
+
+        [Fact]
+        public void FloodDetector_AddEqualToThreshold_ShouldBeFlagged()
+        {
+            var floodDetector = new FloodDetector(60, _threshold);
+
+            foreach (var i in Enumerable.Range(1, _threshold))
+            {
+                floodDetector.Add(_id, DateTime.Now);
+            }
+
+            Assert.True(floodDetector.IsFlooding(_id));
+        }
+
+        [Fact]
+        public void FloodDetector_AddGreaterThanThreshold_ShouldBeFlagged()
+        {
+            var floodDetector = new FloodDetector(60, _threshold);
+
+            foreach (var i in Enumerable.Range(1, _threshold + 1))
+            {
+                floodDetector.Add(_id, DateTime.Now);
+            }
+
+            Assert.True(floodDetector.IsFlooding(_id));
+        }
+    }
+}
