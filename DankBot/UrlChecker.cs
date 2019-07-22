6c3eb9d54e98ed0ecc7b018679db0956569a8aa4
Simon Morgan <sjm@sjm.io>
2019-07-22 19:32:19 +0100

    Add a class for checking URL hosts
    Add a class that checks whether or not a URL's host has been
    whitelisted.

---
+++ DankBot/UrlChecker.cs
@@ -0,0 +1,21 @@
+namespace DankBot
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class UrlChecker
+    {
+        private readonly ICollection<string> _whitelistHosts;
+
+        public UrlChecker(ICollection<string> whitelistHosts)
+        {
+            _whitelistHosts = whitelistHosts;
+        }
+
+        public bool IsWhitelisted(string url)
+        {
+            var uri = new Uri(url);
+            return _whitelistHosts.Contains(uri.Host);
+        }
+    }
+}
