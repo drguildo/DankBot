ca75b3bb4dc0311520ecd431a6086733df7bc9a0
Simon Morgan <sjm@sjm.io>
2019-10-13 12:11:44 +0100

    Add some SpammerDetector tests

---
+++ DankBot.Tests/SpammerDetectorTests.cs
@@ -0,0 +1,27 @@
+﻿namespace DankBot.Tests
+{
+    using Xunit;
+
+    public class SpammerDetectorTests
+    {
+        [Fact]
+        public void SpammerDetector_CheckKnownSpammerId_ShouldReturnTrue()
+        {
+            var spammerDetector = new Helpers.SpammerDetector();
+
+            bool isSpammer = spammerDetector.IsSpammerAsync(821063044).Result;
+
+            Assert.True(isSpammer);
+        }
+
+        [Fact]
+        public void SpammerDetector_CheckNonSpammerId_ShouldReturnFalse()
+        {
+            var spammerDetector = new Helpers.SpammerDetector();
+
+            bool isSpammer = spammerDetector.IsSpammerAsync(46059387).Result;
+
+            Assert.False(isSpammer);
+        }
+    }
+}
